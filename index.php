7b3cdff5eb68bcb36a16734c1e89e943133ad6c9
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-02-10 20:16:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- $json = file_get_contents("https://ex2json.herokuapp.com/json.php");
+ $json = file_get_contents("https://ex2jason.herokuapp.com/json.php");
 
  $data = json_decode($json,true);
  $list = $data['Pets'];
@@ -7,7 +7,7 @@
 
 <center><h1>5 Best Pets to have</h1>
 
-<table border="1px #000 solid">
+<table border="1px">
   <tr class="head">
     <th>Name</th>
     <th>Scientif Name</th>
